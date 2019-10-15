3e878e5cfab9ad4a1fe3249636bfbedb9631cff7
JuliaKazsmerova <julia.kazsmerova@gmail.com>
2019-10-15 13:09:35 +0200

    Added emailExist test, changed empty field tests.

--- webapp/src/test/java/com/gohealth/webapp/SeleniumTests.java
+++ webapp/src/test/java/com/gohealth/webapp/SeleniumTests.java
@@ -3,7 +3,6 @@ package com.gohealth.webapp;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,44 +17,75 @@ public class SeleniumTests {
     private final String email = "Janko.Hrasko@gmail.com";
 
     @Before
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\liagi\\Downloads\\Drivers\\geckodriver.exe");
-        System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
+        System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,
-                System.getProperty("java.io.tmpdir")+"geckodriverlogs.txt");
+                System.getProperty("java.io.tmpdir") + "geckodriverlogs.txt");
         driver = new FirefoxDriver();
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void success(){
-        driver.get("http://localhost:8080/form");
-        driver.findElement(By.id("fname")).sendKeys(fname);
-        driver.findElement(By.id("lname")).sendKeys(lname);
-        driver.findElement(By.id("email")).sendKeys(email);
-        driver.findElement(By.id("submit")).submit();
+    public void success() {
+        fillForm();
 
         WebDriverWait wait = new WebDriverWait(driver, 3);
         assertTrue(wait.until(ExpectedConditions.urlToBe("http://localhost:8080/add")));
     }
 
     @Test
-    public void fNameError(){
+    public void emptyFirstNameError() {
+        emptyFieldError("lname", "email",
+                lname, email, "fnameerror", "first");
+    }
+
+    @Test
+    public void emtyLastNameError() {
+        emptyFieldError("fname", "email",
+                fname, email, "lnameerror", "last");
+    }
+
+    @Test
+    public void emptyEmailError() {
+        emptyFieldError("fname", "lname",
+                fname, lname, "emailerror", "email");
+    }
+
+    @Test
+    public void emailExistsError() {
+        fillForm();
+
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("emailerror")));
+        String message = driver.findElement(By.id("emailerror")).getText();
+        assertTrue(message.contains("exists"));
+
+    }
+
+    private void fillForm(){
         driver.get("http://localhost:8080/form");
+        driver.findElement(By.id("fname")).sendKeys(fname);
         driver.findElement(By.id("lname")).sendKeys(lname);
         driver.findElement(By.id("email")).sendKeys(email);
         driver.findElement(By.id("submit")).submit();
-
-        WebDriverWait wait = new WebDriverWait(driver, 3);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("fnameerror")));
-        String message = driver.findElement(By.id("fnameerror")).getText();
-        assertTrue(message.contains("first"));
     }
 
+    private void emptyFieldError(String elementName1, String elementName2,
+                                 String value1, String value2, String missingElementName, String errorWord) {
 
+        driver.get("http://localhost:8080/form");
+        driver.findElement(By.id(elementName1)).sendKeys(value1);
+        driver.findElement(By.id(elementName2)).sendKeys(value2);
+        driver.findElement(By.id("submit")).submit();
 
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(missingElementName)));
+        String message = driver.findElement(By.id(missingElementName)).getText();
+        assertTrue(message.contains(errorWord));
+    }
 }
